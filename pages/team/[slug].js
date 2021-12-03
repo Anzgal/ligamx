cc9dea495c7bfbd84ffb6af6f610de973ce96a63
Anzgal <agalvan70@hotmail.com>
2021-12-03 11:40:03 -0600

    Minor changes

--- pages/team/[slug].js
+++ pages/team/[slug].js
@@ -100,47 +100,49 @@ export default function Blog({ filteredValue, dataEvents, dataPlayers, dataEquip
         Jerseys
       </p>
       <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {dataEquipment.equipment.map((product) => (
-          <div key={product.idEquipment} className="group relative">
-            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-              <img
-                src={product.strEquipment}
-                alt={product.strType}
-                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-              />
+        {dataEquipment.equipment &&
+          dataEquipment.equipment.map((product) => (
+            <div key={product.idEquipment} className="group relative">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <img
+                  src={product.strEquipment}
+                  alt={product.strType}
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       <div className="space-y-5 my-10 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
         <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Featured players</h2>
       </div>
       <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
-        {dataPlayers.player.map((person) => (
-          <li key={person.idPlayer}>
-            <div className="space-y-4">
-              <div className="aspect-w-3 aspect-h-2">
-                <img
-                  className="object-cover shadow-lg rounded-lg"
-                  src={
-                    person.strThumb
-                      ? person.strThumb
-                      : 'https://thumbs.dreamstime.com/b/profile-picture-vector-perfect-social-media-other-web-use-125320944.jpg'
-                  }
-                  alt={person.strPlayer}
-                />
-              </div>
+        {dataPlayers.player &&
+          dataPlayers.player.map((person) => (
+            <li key={person.idPlayer}>
+              <div className="space-y-4">
+                <div className="aspect-w-3 aspect-h-2">
+                  <img
+                    className="object-cover shadow-lg rounded-lg"
+                    src={
+                      person.strThumb
+                        ? person.strThumb
+                        : 'https://thumbs.dreamstime.com/b/profile-picture-vector-perfect-social-media-other-web-use-125320944.jpg'
+                    }
+                    alt={person.strPlayer}
+                  />
+                </div>
 
-              <div className="space-y-2">
-                <div className="text-lg leading-6 font-medium space-y-1">
-                  <h3>{person.name}</h3>
-                  <p className="text-indigo-600">{person.strPlayer}</p>
+                <div className="space-y-2">
+                  <div className="text-lg leading-6 font-medium space-y-1">
+                    <h3>{person.name}</h3>
+                    <p className="text-indigo-600">{person.strPlayer}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))}
       </ul>
 
       <div className="relative  ">
@@ -149,27 +151,28 @@ export default function Blog({ filteredValue, dataEvents, dataPlayers, dataEquip
             <p className="mt-2 text-3xl font-extrabold tracking-tight sm:text-4xl">Last matches</p>
           </div>
           <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
-            {dataEvents.results.map((post) => (
-              <div
-                key={post.idEvent}
-                className="flex flex-col rounded-lg shadow-lg overflow-hidden"
-              >
-                <div className="flex-shrink-0">
-                  <img className="h-48 w-full object-cover" src={post.strThumb} alt="" />
-                </div>
-                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                  <div className="flex-1">
-                    <p className="text-sm font-medium text-indigo-600">{post.strEvent}</p>
-                    <div className="block mt-2">
-                      <p className="text-xl font-semibold text-gray-900">
-                        {post.intHomeScore} - {post.intAwayScore}
-                      </p>
-                      <p className="mt-3 text-base text-gray-500">{post.dateEventLocal}</p>
+            {dataEvents.results &&
+              dataEvents.results.map((post) => (
+                <div
+                  key={post.idEvent}
+                  className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+                >
+                  <div className="flex-shrink-0">
+                    <img className="h-48 w-full object-cover" src={post.strThumb} alt="" />
+                  </div>
+                  <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                    <div className="flex-1">
+                      <p className="text-sm font-medium text-indigo-600">{post.strEvent}</p>
+                      <div className="block mt-2">
+                        <p className="text-xl font-semibold text-gray-900">
+                          {post.intHomeScore} - {post.intAwayScore}
+                        </p>
+                        <p className="mt-3 text-base text-gray-500">{post.dateEventLocal}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </div>
